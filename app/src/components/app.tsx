7e247ce56b867ba21c298964816b463ccbcd154f
James Hanselman <68711757+JamesHansIV@users.noreply.github.com>
2025-04-05 00:55:22 -0500

    initial dialog box

--- app/src/components/app.tsx
+++ app/src/components/app.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+
+const App: React.FC = () => {
+    const [outputs, setOutputs] = useState<string[]>([]);
+    const [cmdText, setCmdText] = useState<string>('');
+
+    const handleClick = () => {
+        setOutputs([...outputs, cmdText]);
+    }
+
+    const cmdTextInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCmdText(e.target.value);
+    }
+
+    return (
+        <div>
+            {/* output div */}
+            <div>
+                {
+                    outputs.map((output, index) => (
+                        <p><b>{index}:</b> {output}</p>
+                    ))
+                }
+            </div>
+
+            {/* input div */}
+            <div>
+                <input value={cmdText} onChange={cmdTextInputHandler}/>
+                <button onClick={handleClick}>Exec</button>
+            </div>
+        </div>
+    );
+};
+
+root.render(<App/>);
