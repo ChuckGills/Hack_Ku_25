42fc6cb75f5093dd5a77a1a7bef8c49a5b81598e
James Hanselman <68711757+JamesHansIV@users.noreply.github.com>
2025-04-06 05:33:04 -0500

    pull requests

--- app/src/components/PullRequests.tsx
+++ app/src/components/PullRequests.tsx
@@ -9,21 +9,56 @@ interface PullRequest {
     author: string;
     number: number;
     title: string;
+    open?: boolean;
 }
 export const PullRequests: React.FC<PullRequests> = ({repo}) => {
-    const [prs, setPrs] = useState<PullRequest[]>([]);
+    const [openPrs, setOpenPrs] = useState<PullRequest[]>([]);
+    const [closedPrs, setClosedPrs] = useState<PullRequest[]>([]);
 
     useEffect(()=>{
-        const fetchRepo = async () => {
-            const result = await window.myAPI.runCommand(`pr list`);
-            console.log(result);
+        const getPrList = async () => {
+            const data = await window.myAPI.runCommand(`pr list`);
+            console.log('data',data);
+            
+            // const parsedData = JSON.parse(data);
+
+            // Map the data to fit the PullRequest interface
+            const openPrArr: PullRequest[] = data.open.map((pr: any) => ({
+                author: pr.author.login, // Extract the login as the author's name
+                number: pr.number,
+                title: pr.title,
+            }));
+
+            const closedPrArr: PullRequest[] = data.closed.map((pr: any) => ({
+                author: pr.author.login, // Extract the login as the author's name
+                number: pr.number,
+                title: pr.title,
+            }));
+            setOpenPrs(openPrArr);
+            setClosedPrs(closedPrArr);
+            // return result;
         }
-        fetchRepo();
+        getPrList();
     },[repo])
 
     return (
         <div>
-            {/* < */}
+            {
+                openPrs.map((pr, index)=>(
+                    <div>
+                        <span style={{color:'#33ff33'}}>{`open [${pr.number}] ${pr.author}: `}</span>
+                        <span style={{color:'white'}}>{`${pr.title}`}</span>
+                    </div>
+                ))
+            }
+            {
+                closedPrs.map((pr, index)=>(
+                    <div>
+                        <span style={{color:'red'}}>{`closed [${pr.number}] ${pr.author}: `}</span>
+                        <span style={{color:'white'}}>{`${pr.title}`}</span>
+                    </div>
+                ))
+            }
         </div>
     );
 }
