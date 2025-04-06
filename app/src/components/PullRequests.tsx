f26244ef77cc1a8beb149470df3736bcc6e65efa
PDykes1996 <pdykes1996@gmail.com>
2025-04-06 06:18:55 -0500

    PR List Style

--- app/src/components/PullRequests.tsx
+++ app/src/components/PullRequests.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Repo } from "./RepoCard";
+import styles from "./PullRequests.module.css";
 
 interface PullRequestsProps {
     repo: Repo,
@@ -51,16 +52,20 @@ export const PullRequests: React.FC<PullRequestsProps> = ({ repo, sendDataToPare
         <div>
             {
                 openPrs.map((pr, index) => (
-                    <div onClick={() => selectPr(pr)}>
-                        <span style={{ color: '#33ff33' }}>{`open [${pr.number}] ${pr.author}: `}</span>
+                    <div className={styles.openPullRequest} onClick={() => selectPr(pr)}>
+			<span style={{ color: 'grey' }}> { `[${pr.number}] > ` }</span>
+                        <span style={{ color: '#33ff33' }}>{`open`}</span>
+			<span style={{ color: 'yellow' }}> {`${pr.author}: `}</span>
                         <span style={{ color: 'white' }}>{`${pr.title}`}</span>
                     </div>
                 ))
             }
             {
                 closedPrs.map((pr, index) => (
-                    <div onClick={() => selectPr(pr)}>
-                        <span style={{ color: 'red' }}>{`closed [${pr.number}] ${pr.author}: `}</span>
+                    <div className={styles.closedPullRequest} onClick={() => selectPr(pr)}>
+			<span style={{ color: 'grey' }}> { `[${pr.number}] > ` }</span>
+                        <span style={{ color: 'red' }}>{`closed`}</span>
+		    	<span style={{ color: 'yellow' }}> {`${pr.author}: `}</span>
                         <span style={{ color: 'white' }}>{`${pr.title}`}</span>
                     </div>
                 ))
